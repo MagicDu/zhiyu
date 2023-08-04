4b274fa18c98740f313524b0420e7e8df30f8e34
magicdu <2760522291@qq.com>
2023-08-04 10:35:28 +0800

    :boom: add socket user service

---
+++ zhiyu-system/src/main/java/com/zhiyu/system/service/ActiveWebSocketUserService.java
@@ -0,0 +1,8 @@
+package com.zhiyu.system.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.zhiyu.common.core.entity.ActiveWebSocketUser;
+import com.zhiyu.system.mapper.ActiveWebSocketUserMapper;
+
+public interface ActiveWebSocketUserService extends IService<ActiveWebSocketUser> {
+}
